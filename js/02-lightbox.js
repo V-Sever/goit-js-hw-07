dffad52759c72cc1109aaab873cb9fbcb91f909e
Viacheslav Severyn <sever_2008@ukr.net>
2022-02-12 16:26:01 +0200

    hw-07_done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,16 +5,13 @@ import { galleryItems } from './gallery-items.js';
 
 const refs = {
   gallery: document.querySelector('.gallery'),
-  galleryLinks: [],
 };
-
-let bigImageURL = null;
 
 function render(arr) {
   const markup = arr
     .map(
       elmnt =>
-        `<a class="gallery__item" href="${elmnt.original}"><img class="gallery__image" src="${elmnt.preview}" alt="${elmnt.description}" /></a>`
+        `<a class="gallery__item" href="${elmnt.original}"><img class="gallery__image" src="${elmnt.preview}" alt="${elmnt.description}" title="${elmnt.description}" /></a>`
     )
     .join('');
   refs.gallery.insertAdjacentHTML('beforeend', markup);
@@ -22,19 +19,4 @@ function render(arr) {
 
 render(galleryItems);
 
-refs.galleryLinks = document.querySelectorAll('.gallery__item');
-
-refs.galleryLinks.forEach(link => link.addEventListener('click', onLinkClick));
-
-function onLinkClick(evt) {
-  evt.preventDefault();
-  bigImageURL = evt.currentTarget.getAttribute('href');
-}
-
-refs.gallery.addEventListener('click', onClick);
-
-function onClick(evt) {
-  //   console.log(evt.target);
-  if (!evt.target.classList.contains('gallery__image')) return;
-  console.log(bigImageURL);
-}
+let gallery = new SimpleLightbox('.gallery a', { captionDelay: 250 });
